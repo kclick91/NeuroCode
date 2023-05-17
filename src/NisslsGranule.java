9f16d6f6aeff5bc7ab15756df1f409ad60f9c45e
kclick91 <kclick91@gmail.com>
2023-05-17 15:23:27 -0400

    Correction/Addition to NisslsGranule class

---
+++ src/NisslsGranule.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class NisslsGranule extends Organelle {
+
+    EndoplasmicReticulum er;
+
+    public NisslsGranule()
+    {
+
+    }
+
+    public void SynthesizeProteins()
+    {
+        
+    }
+
+
+}
